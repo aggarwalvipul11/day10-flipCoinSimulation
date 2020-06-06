1feb5244d13ecaca007d1d5427fa512639094deb
aggarwalvipul11 <vipul_dev@DESKTOP-QA3S37M.localdomain>
2020-06-06 09:11:45 +0530

    Completed Usercase01

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,29 +1,9 @@
-flip()
-{
-    if [[ $((RANDOM%2)) == 0 ]]
-    then
-        echo "heads"
-    else
-        echo "tails"
-    fi
-}
+#!/bin/bash -x
 
-a[0]=0
-a[1]=0
-while [ ${a[0]} -lt 11 ] &&  [ ${a[1]} -lt 11 ]
-do  
-    temp=$(flip)
-    if [[ $temp == 'heads' ]]
-    then
-        a[0]=$((${a[0]}+1))
-    else
-        a[1]=$((${a[1]}+1))
-    fi
-done
-echo "score of heads is ${a[0]} and tails is ${a[1]}"
-if [[ ${a[0]} -gt ${a[1]} ]]
+if [[ $((RANDOM%2)) == 0 ]]
 then
-    echo "heads is winner by $(echo $((${a[0]}-${a[1]})))"
-else 
-    echo "tails is winner by $(echo $((${a[1]}-${a[0]})))"
+    echo "heads"
+else
+     echo "tails"
 fi
+
