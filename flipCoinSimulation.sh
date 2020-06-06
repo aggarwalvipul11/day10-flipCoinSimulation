15ace25b5d35d44b66ff1a77018599d45747416d
aggarwalvipul11 <vipul_dev@DESKTOP-QA3S37M.localdomain>
2020-06-06 09:18:13 +0530

    Completed Usercase02

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,9 +1,21 @@
 #!/bin/bash -x
 
-if [[ $((RANDOM%2)) == 0 ]]
-then
-    echo "heads"
-else
-     echo "tails"
-fi
+function flip()
+{
+    if [[ $((RANDOM%2)) == 0 ]]
+    then
+        echo "heads"
+    else
+        echo "tails"
+    fi
+}
 
+a[0]=0
+a[1]=0
+temp=$(flip)
+if [[ $temp == 'heads' ]]
+then
+    a[0]=$((${a[0]}+1)
+else
+    a[1]=$((${a[1]}+1))
+fi
