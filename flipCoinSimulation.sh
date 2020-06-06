871ef16ae20e254c538bb74e67e906d95a4f8b6e
aggarwalvipul11 <vipul_dev@DESKTOP-QA3S37M.localdomain>
2020-06-06 09:25:24 +0530

    Completed UC3

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,9 +1,25 @@
 #!/bin/bash -x
 
-if [[ $((RANDOM%2)) == 0 ]]
-then
-    echo "heads"
-else
-     echo "tails"
-fi
+function flip()
+{
+    if [[ $((RANDOM%2)) == 0 ]]
+    then
+        echo "heads"
+    else
+        echo "tails"
+    fi
+}
 
+a[0]=0
+a[1]=0
+while [ ${a[0]} -lt 21 ] &&  [ ${a[1]} -lt 21 ]
+do  
+    temp=$(flip)
+    if [[ $temp == 'heads' ]]
+    then
+        a[0]=$((${a[0]}+1))
+    else
+        a[1]=$((${a[1]}+1))
+    fi
+done
+echo "score of heads is ${a[0]} and tails is ${a[1]}"
